1fab7e20f5c190944fe66feaa40229cc7addb081
mati-nvidia <mcodesal@nvidia.com>
2022-10-31 18:32:18 -0400

    bug fixes

--- source/extensions/my_name.my_app.window/my_name/my_app/window/extension.py
+++ source/extensions/my_name.my_app.window/my_name/my_app/window/extension.py
@@ -1,6 +1,6 @@
 import omni.ext
 import omni.kit.ui
-# import omni.ui as ui
+
 from .window import MyWindow
 
 
@@ -15,12 +15,12 @@ class WindowExtension(omni.ext.IExt):
         # Most application have an Window Menu, See MenuLayout to re-organize it
         self._window = None
         self._menu = editor_menu.add_item(WindowExtension.MENU_PATH, self._on_menu_click, toggle=True, value=True)
-        self.show_window(self._menu, True)
+        self.show_window(True)
 
     def _on_menu_click(self, menu, toggled):
-        self.show_window(menu, toggled)
+        self.show_window(toggled)
 
-    def show_window(self, menu, toggled):
+    def show_window(self, toggled):
         if toggled:
             if self._window is None:
                 self._window = MyWindow()
@@ -28,12 +28,19 @@ class WindowExtension(omni.ext.IExt):
             else:
                 self._window.show()
         else:
-            self._window = None
+            if self._window is not None:
+                self._window.hide()
 
     def _visiblity_changed_fn(self, visible):
         if self._menu:
             omni.kit.ui.get_editor_menu().set_value(WindowExtension.MENU_PATH, visible)
-            self.show_window(None, visible)
+            self.show_window(visible)
 
     def on_shutdown(self):
-        self._window = None
+        if self._window is not None:
+            self._window.destroy()
+            self._window = None
+        if self._menu is not None:
+            editor_menu = omni.kit.ui.get_editor_menu()
+            editor_menu.remove_item(self._menu)
+            self._menu = None
