9a1c916e362860415ddb8df364890238521088b8
Greg Amato <gamato@nvidia.com>
2026-07-15 12:35:16 -0400

    fix: remove sample applications from premake and repo.toml

--- premake5.lua
+++ premake5.lua
@@ -15,5 +15,3 @@ repo_build.prebuild_copy {
 }
 
 -- Apps: for each app generate batch files and a project based on kit files (e.g. my_name.my_app.kit)
-
-define_app("my_company.my_usd_composer.kit")
